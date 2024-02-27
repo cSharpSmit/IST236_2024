a0c963ad90d3a44299ab022f152eb64bf4573930
cSharpSmit <cwscowboy1998@gmail.com>
2024-02-27 00:28:00 -0500

    Finished Sandwich App, does not clear when home button is pressed

---
+++ Week 7/SandwichShop/components/Title.js
@@ -0,0 +1,21 @@
+import { Text, StyleSheet } from "react-native";
+import Colors from "../constants/colors";
+// ^^ Imports components for use ^^ \\
+
+// Defines a simple functional component "Title" that takes props (properties) as its parameter
+// This component is designed to display a text title, where the text content is passed as children
+function Title(props){
+    return <Text style={styles.title}>{props.children}</Text>;
+}
+
+// Exports the component for modular use
+export default Title;
+
+// Style sheet for the title
+const styles = StyleSheet.create({
+    title:{
+        fontSize: 50,
+        color: Colors.primary500,
+        textAlign: "center"
+    }
+});
---
+++ Week 7/SandwichShop/constants/colors.js
@@ -0,0 +1,9 @@
+const Colors = {
+    accent300: "#007a33",
+    primary300: "#ffffff",
+    primary500: "#f9d616",
+    primary800: "#000000",
+};
+
+// Exports the component for modular use
+export default Colors;
